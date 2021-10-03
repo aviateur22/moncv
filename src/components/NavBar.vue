fe4a135f5ca61a41632200f0cae7dc55a2be59ca
cyrille <aviateur22@hotmail.fr>
2021-10-03 21:01:19 +0200

    add header

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="container">
+    <header class="header">      
+        <nav class="header_navbar">
+            
+        </nav>
+  </header>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
